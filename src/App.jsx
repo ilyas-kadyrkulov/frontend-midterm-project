f22b23266eb7ad82e4da30dc1721a63897dc8854
ILIAS KADYRKULOV <ilias.kadyrkulov@gmail.com>
2022-11-05 20:15:34 +0600

    whole project upload

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import TextArea from './components/TextArea'
+
+function App() {
+ 
+  return (
+    <div className="App">
+      <div className="main">
+        <TextArea />
+        <TextArea />
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/TextArea.jsx
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from 'react'
+import './index.css'
+
+const TextArea = () => {
+    const [message, setMessage] = useState('')
+    const [theme, setTheme] = useState('')
+    const [typing, setTyping] = useState('')
+
+    if (!message || message === '\n') {
+        setMessage('Change the theme, typing in the textarea, and pressing Enter.')
+    }
+
+    const handleSubmit = () => {
+        setTheme(message)
+    }
+
+    const handleMessageChange = (event) => {
+        setTimeout(() => {
+            setMessage(event.target.value)
+        }, 300)
+
+    }
+
+    const handleTyping = () => {
+        setTyping('typing...')
+    }
+
+    return (
+        <div className='message'>
+            <div className='message__content'>
+                <span>Theme: {theme}</span>
+                <p>{message}</p>
+            </div>
+            <div className='textarea'>
+                <textarea
+                    id="message"
+                    name="message"
+                    onChange={handleMessageChange}
+                    onKeyPress={(e) => {
+                        if (e.key === 'Enter' && !message.endsWith('Enter.')) handleSubmit()
+                        handleTyping()
+                    }}
+                />
+            <span>{typing}</span>
+            </div>
+        </div>
+    )
+}
+
+export default TextArea
